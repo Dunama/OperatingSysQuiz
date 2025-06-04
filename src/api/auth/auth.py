1398eb3eb4e0365b5feffcff2e51ba8ffb61126b
DUNAMA DAHIRU <148614497+Dunama@users.noreply.github.com>
2025-06-04 01:17:48 +0100

    auth routes

--- src/api/auth/auth.py
+++ src/api/auth/auth.py
@@ -9,10 +9,11 @@ auth_bp = Blueprint('auth', __name__)
 
 # google config
 google_bp = make_google_blueprint(
-    client_id = os.getenv("GOOGLE_CLIENT_ID"),
-    client_secret = os.getenv("GOOGLE_CLIENT_SECRET"),
-    scope = ["profile","email"],
-    redirect_url = os.getenv("GOOGLE_REDIRECT_URI")  # Use the environment variable
+    client_id=os.getenv("GOOGLE_CLIENT_ID"),
+    client_secret=os.getenv("GOOGLE_CLIENT_SECRET"),
+    scope=["profile", "email"],
+    redirect_to="/login/google/callback",  # Update this
+    authorized_url="/login/google/authorized"  # Add this
 )
 
 @auth_bp.route("/signup")
