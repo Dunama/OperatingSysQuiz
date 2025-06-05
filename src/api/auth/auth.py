08a635d4d1b12f86480b71d9e39d044addd6b73e
DUNAMA DAHIRU <148614497+Dunama@users.noreply.github.com>
2025-06-05 11:59:35 +0100

    OS quiz

--- src/api/auth/auth.py
+++ src/api/auth/auth.py
@@ -1,15 +1,15 @@
 from flask import Blueprint, redirect, url_for, render_template, session
 from authlib.integrations.flask_client import OAuth
 import json
-
+import os
 
 auth_bp = Blueprint('auth', __name__)
 appConfig = {
-    "OAUTH2_CLIENT_ID": "883508464308-i5uf96m77mhubdfaqkqa4qc92i1nng27.apps.googleusercontent.com",
-    "OAUTH2_CLIENT_SECRET": "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",
-    "OAUTH2_METADATA_URL": "https://accounts.google.com/.well-known/openid-configuration",
-    "FLASK_PORT": 5000
+    "OAUTH2_CLIENT_ID": os.getenv("OAUTH2_CLIENT_ID"),
+    "OAUTH2_CLIENT_SECRET": os.getenv("OAUTH2_CLIENT_SECRET"),
+    "OAUTH2_METADATA_URL": os.getenv("OAUTH2_METADATA_URL")
 }
+
 oauth = OAuth()
 
 
