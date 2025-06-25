8b6881cd4dbaa0b7fb20aa7112fbe2a2763eb738
DUNAMA DAHIRU <148614497+Dunama@users.noreply.github.com>
2025-06-25 22:39:06 +0100

    ren

--- wsgi.py
+++ wsgi.py
@@ -1,3 +1,14 @@
+import os
+import sys
+
+# Add the project root to Python path
+sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
+
+# Suppress SQLAlchemy typing warnings
+import warnings
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+warnings.filterwarnings("ignore", message=".*TypingOnly.*")
+
 from app import create_app
 
 app = create_app()
